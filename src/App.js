4de965ec3caab44d7e83c95901ccbe20caa2842d
IlCarra <stevan.carrara@gmail.com>
2025-02-18 19:00:00 +0100

    added footer and made last modification

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import CustomNavBar from './pages/navbar';
+import CustomNavBar from './components/navbar';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/home';
-import About from './pages/apropos';
+
 
 function App() {
   return (
@@ -10,7 +10,6 @@ function App() {
       <CustomNavBar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/apropos' element={<About />} />
 
       </Routes>
     </div>
