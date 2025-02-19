399438ab48fd4b0cbbc6efd0d22f054582b4e22a
IlCarra <stevan.carrara@gmail.com>
2025-02-19 11:20:27 +0100

    added contact page and contact-form component

---
+++ src/components/contact-form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ContactForm() {
+    return (
+        <form>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Votre Nom" />
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Votre adresse mail" />
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Votre numéro de téléphone" />
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Sujet" />
+    </form>
+    );
+};
+
+export default ContactForm;
