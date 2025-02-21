77062ba206c1b4778d406ab5b3ead07cee8a4859
IlCarra <stevan.carrara@gmail.com>
2025-02-21 10:46:01 +0100

    styled the footer and added the links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,46 +1,88 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <footer className="py-5 border-top bg-dark text-light w-100" data-bs-theme="dark" >
+        <div className="container-fluid footer-container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-5 container-fluid ">
+            <div className="col mb-3 my-column">
+              <h5>Stevan Carrara</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">40 Rue Laure Diebold</li>
+                <li className="nav-item mb-2">69009 Lyon, France</li>
+                <li className="nav-item mb-2">10 20 30 40 50 60</li>
+                <li className="nav-item mb-2">stevan.carrara@gmail.com</li>
+                <li className="nav-item mb-2">
+                    <ul className="list-inline">
+                        <li className="list-inline-item">
+                            <a href="https://github.com/IlCarra" className="nav-link text-light" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-github"></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="https://x.com/i/flow/signup" className="nav-link text-light" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-twitter"></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="https://www.linkedin.com/in/stevancarrara/" className="nav-link text-light" target="_blank" rel="noopener noreferrer">
+                                <i class="bi bi-linkedin"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+              </ul>
+            </div>
 
-    <div class="col mb-3">
 
-    </div>
+            <div className="col mb-3 my-column">
+              <h5>Liens utiles</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="/">Accueil </ NavLink> 
+                </li>
+                <li className="nav-item mb-2"> 
+                    <NavLink className='f-link' to="../pages/service">Services </ NavLink>
+                </li>
+                <li className="nav-item mb-2"> 
+                    <NavLink className='f-link' to="../pages/portfolio">Portfolio </ NavLink>
+                </li>
+                <li className="nav-item mb-2"> 
+                    <NavLink className='f-link' to="../pages/contact">Me contacter </ NavLink>
+                </li>
+                <li className="nav-item mb-2"> 
+                    <NavLink className='f-link' to="../pages/mentions-legales">Mentions Légales </NavLink> 
+                </li>
+              </ul>
+            </div>
 
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
-    </div>
-
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
-    </div>
-
-    <div class="col mb-3">
-      <h5>Section</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-      </ul>
-    </div>
-  </footer>
+            <div className="col mb-3 my-column">
+              <h5>Mes dernières réalisations</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="../pages/portfolio">Fresh Food </ NavLink>
+                </li>
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="../pages/portfolio">Restaurant Akira </ NavLink>
+                </li>
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="../pages/portfolio">Espace bien-etre </ NavLink>
+                </li>
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="../pages/portfolio">SEO </ NavLink>
+                </li>
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="../pages/portfolio">Création d'une API </ NavLink>
+                </li>
+                <li className="nav-item mb-2">
+                    <NavLink className='f-link' to="../pages/portfolio">Maquette d'un site </ NavLink>
+                </li>
+              </ul>
+            </div>
+        </div>
+        </div>
+    </footer>
     );
 };
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function CustomNavBar() {
 return (
     <nav class="navbar bg-dark navbar-expand-lg border-bottom border-body" data-bs-theme="dark">
-        <div class="container-fluid">
+        <div class="container-fluid nav-container">
           <a class="navbar-brand" href='/'>Stevan Carrara</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -12,19 +12,19 @@ return (
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
               <li className='nav-item'>
-                <Link className='link' to="/">Home </ Link>
+                <NavLink className='link' activeClassName='active' to="/">Home </ NavLink>
               </li>
               <li className='nav-item'>
-                <Link className='link' to="../pages/service">Services </ Link>
+                <NavLink className='link' activeClassName='active' to="../pages/service">Services </ NavLink>
               </li>
               <li className='nav-item'>
-                <Link className='link' to="../pages/portfolio">Portfolio </ Link>
+                <NavLink className='link' activeClassName='active' to="../pages/portfolio">Portfolio </ NavLink>
               </li>
               <li className='nav-item'>
-                <Link className='link' to="../pages/contact">Contact </ Link>
+                <NavLink className='link' activeClassName='active' to="../pages/contact">Contact </ NavLink>
               </li>
               <li className='nav-item'>
-                <Link className='link' to="../pages/mentions-legales">Mentions Légales</Link>
+                <NavLink className='link' activeClassName="active" to="../pages/mentions-legales">Mentions Légales</NavLink>
               </li>
             </ul>
           </div>
