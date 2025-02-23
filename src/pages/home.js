20ee6795777389642ba282feb5ae021d7ef4ff89
IlCarra <stevan.carrara@gmail.com>
2025-02-23 16:50:11 +0100

    last changement

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ import './home.css';
 
 const Home = () => {
     return (
-    <main>
+    <div>
         <div className='Background'>
           <h1>Bonjour je suis Stevan Carrara</h1>
           <h2>Développeur Full-Stack</h2>
@@ -14,10 +14,10 @@ const Home = () => {
         </div>
         <div className='profile-container'>
           <div className='profile'>
-            <div className='aboutMe'>
+            <div className='col aboutMe'>
               <h2>A propos</h2>
               <div className='homeLine'></div>
-              <img src="/images/AboutMe.jpg" alt='Développer'></img>
+              <img src="/images/AboutMe.jpg" alt='Développer' className='img-fluid'></img>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquam malesuada cursus. 
                 Quisque eu mi dolor. Quisque sit amet tortor non tortor aliquam tempus. Sed diam lectus, 
@@ -29,14 +29,14 @@ const Home = () => {
                 Suspendisse potenti. Proin nec orci ac metus luctus laoreet.
               </p>
             </div>
-            <div className='Skills'>
+            <div className='col Skills'>
               <h2>Mes competences</h2>
-            <div className='homeLine'></div>
+              <div className='homeLine'></div>
               <ProgressBars />
             </div>
           </div>
         </div>
-    </main>
+    </div>
   );
 };
 
